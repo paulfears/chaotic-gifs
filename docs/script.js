25e87c7c87b913e5b6f3de024c6dbfd9b3e9b57e
paulfears <paulrfears@gmail.com>
2019-02-05 12:41:43 -0800

    added colors fixed bugs

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,22 @@
 images = []
 let go = true
 
-function generate(args = [2.24, 0.43, -.65, -2.43, 1]){
+function generate(args = [2.24, 0.43, -.65, -2.43, 1, 0]){
   
   let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext('2d');
   ctx.clearRect(0,0,canvas.width, canvas.height);
   ctx.fillStyle = "white";
   ctx.fillRect(0,0, canvas.width, canvas.height);
-  ctx.fillStyle = "black";
+  
+  let color = Number(args[5]).toString(16);
+
+  let times = 6-color.length;
+  for(let i = 0; i<times; i++){
+    color = "0"+color;
+  }
+  color = "#"+color;
+  ctx.fillStyle = color;
   let xmin = -2
   let xmax = 2
   let ymin = -2
@@ -47,10 +55,10 @@ function generate(args = [2.24, 0.43, -.65, -2.43, 1]){
 function create_gif(delay = 100, bg_color = "white"){
   return new Promise(function(resolve, reject){
 
-    let frames = document.getElementsByClassName('frame');
+    let images = document.getElementsByClassName('frame-image');
     let gif = new GIF({workers:2, quality:2, width:600, height:600, bg_color:bg_color});
-    for(let i = 1; i<frames.length; i++){
-      gif.addFrame(frames[i], {delay: delay});
+    for(let i = 1; i<images.length; i++){
+      gif.addFrame(images[i], {delay: delay});
     }
     gif.on('finished', function(blob){
       resolve(URL.createObjectURL(blob));
@@ -60,28 +68,37 @@ function create_gif(delay = 100, bg_color = "white"){
   });
 }
 
+function display_progress(){
+  let output = document.getElementsByClassName("image-output")[0];
+  output.style.display = "block";
+  let progress = document.getElementsByClassName('progress')[0];
+  progress.style.display = "block";
+}
+function hide_progress(){
+  let progress = document.getElementsByClassName('progress')[0];
+  progress.style.display = "none";  
+}
+function open_viewer(gif){
+  let viewer = document.getElementsByClassName("gif-viewer")[0];
+  let output_image_element = document.getElementById("display-image");
+  viewer.style.display = "block";
+  output_image_element.src = gif;
+}
+
 function build_gif(){
-	let all_content = document.getElementsByClassName('main-content')[0];
-	let progress = document.getElementsByClassName('progress')[0];
-    all_content.style.display = "none";
-    progress.style.display = "block";
+  display_progress();
   create_gif().then(
   	function(new_gif){
-  	  progress.display = "none";
-      let output = document.getElementsByClassName("image-output")[0];
-      let output_image_element = document.getElementById("display-image");
-      progress.style.display = "none";
-      output.style.display = "block";
-      output_image_element.src = new_gif;
+      hide_progress();
+      open_viewer(new_gif);
   }
   );
 }
 
 function exit_viewer(){
   let output = document.getElementsByClassName("image-output")[0];
-  let output_image_element = document.getElementById("display-image");
-  let all_content = document.getElementsByClassName('main-content')[0];
-  all_content.style.display = "grid";
+  let viewer = document.getElementsByClassName("gif-viewer")[0];
+  viewer.style.display = "none"; 
   output.style.display = "none";
 }
 
@@ -93,7 +110,6 @@ exit_viewer_button.addEventListener('click', exit_viewer);
 function get_computed_style(element, propery_name){
   return new Promise(function(resolve, reject){
     let propery = window.getComputedStyle(element, null).getPropertyValue(propery_name);
-    console.log("done")
     window.setTimeout(()=>resolve(propery), 140);
   });
 }
@@ -102,19 +118,19 @@ function add_frame(){
   let src = canvas.toDataURL('image/png');
   let frames_container = document.getElementById("frames");
   let scroller = document.getElementsByClassName("frames-holder")[0];
-  let img = document.getElementsByClassName("frame")[0];
-  img = img.cloneNode(true);
-  img.style.display = "inline-block";
-  img.src = src;
+  let frame = document.getElementsByClassName("frame")[0];
+  new_frame = frame.cloneNode(true);
+  new_frame.style.display = "inline-block";
+  new_frame.children[0].src = src;
   get_computed_style(frames_container, "height").then(function(width){
-    img.style.width = width;
-    frames_container.appendChild(img);
+    new_frame.style.width = width;
+    frames_container.appendChild(new_frame);
     scroller.scrollLeft = scroller.scrollWidth;
   });
 }
 
 
-let args = [2.24, 0.43, -0.65, -2.43, 1]
+let args = [2.24, 0.43, -0.65, -2.43, 1, 0]
 
 function update_args(arg_index, value){
   args[arg_index] = value;
@@ -135,16 +151,53 @@ function decrement(button_index){
   update_args(button_index, sliders[button_index].value);
 }
 
+function delete_frame(button_element){
+  console.log("called");
+  let top_div = button_element.parentNode.parentNode;
+  top_div.removeChild(button_element.parentNode);
+}
+
 let sliders = document.getElementsByClassName("slider");
 let arg_displays = document.getElementsByClassName("arg_disp");
 let adders = document.getElementsByClassName("plus_button");
 let subers = document.getElementsByClassName("minus_button");
 
+
+
+function to_hex(number){
+    let color = Number(number).toString(16);
+
+  let times = 6-color.length;
+  for(let i = 0; i<times; i++){
+    color = "0"+color;
+  }
+  color = "#"+color;
+  return color;
+
+}
+
 for(let i = 0; i<sliders.length; i++){
+
   sliders[i].addEventListener('change', ()=>update_args(i, sliders[i].value));
-  sliders[i].addEventListener('input', ()=>update_arg_disp(i, sliders[i].value));
-  adders[i].addEventListener('click', ()=>increment(i));
-  subers[i].addEventListener('click', ()=>decrement(i));
+  if(i == 5){
+    sliders[i].addEventListener('input', ()=>update_arg_disp(i, to_hex(sliders[i].value)));
+    adders[i].addEventListener('click', ()=>{
+      sliders[i].value+=1;
+      update_arg_disp(i, to_hex(sliders[i].value));
+      update_args(i, sliders[i].value);
+    });
+    subers[i].addEventListener('click', ()=>{
+      sliders[i].value-=1;
+      update_arg_disp(i, to_hex(sliders[i].value));
+      update_args(i, sliders[i].value);
+    });
+  }
+  else{
+    sliders[i].addEventListener('input', ()=>update_arg_disp(i, sliders[i].value));
+    adders[i].addEventListener('click', ()=>increment(i));
+    subers[i].addEventListener('click', ()=>decrement(i));
+  }
+
 }
 
 function resize(){
